d950f413213d87474f29d88d8a42c1988d329e94
HouseWei <1151685042@qq.com>
2020-07-18 15:27:50 +0800

    mock

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <el-card>
+      <el-button type="primary" @click="submitForm('ruleForm')">立即获取</el-button>
+    </el-card>
+    <el-card>
+      <el-table :data="tableData" border stripe style="width:100%">
+        <el-table-column
+          v-for="(item,index) in tHeaders"
+          :key="index"
+          :prop="item"
+          :label="item"
+          align="center"
+        ></el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      ruleForm: {},
+      rules: {},
+      tableData: []
+    }
+  },
+  computed: {
+    tHeaders () {
+      if (!this.tableData[0]) return
+      var arr = []
+      Object.keys(this.tableData[0]).forEach(item => {
+        arr.push(item)
+      })
+      return arr
+    }
+  },
+  created () {},
+  methods: {
+    submitForm (form) {
+      axios
+        .get('/user/produce', this.ruleForm)
+        .then(res => {
+          if (res.status === 200) {
+            this.$message.success('获取成功')
+          }
+          console.log(res)
+          this.tableData = res.data.tableData
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  .el-card {
+    margin-bottom: 15px;
+  }
+}
+</style>
